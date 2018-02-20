91990a625303d2d4463093cbf08883166016f521
masupial <mali@cs.toronto.edu>
2018-02-20 14:48:50 -0500

    visualising the samples as a plot
    alight modification to previous implementation, now the plotting is
    done as a separate function

--- ReadData.py
+++ ReadData.py
@@ -5,19 +5,23 @@ def read_wav_file(file_name):
     '''
     function to read the wav file
     '''
-
     sample_rate, data = wavfile.read(file_name)
+    return sample_rate, data
 
-    # optional code for plotting of the signal
+def plot_data(data):
+    '''
+    function to plot the wav file
+    '''
     plt.figure(1)
     plt.title('speech wave')
     plt.plot(data)
     plt.show()
 
 
-    return sample_rate, data
-
-
 if __name__ == '__main__':
     sample_rate, data = read_wav_file('harvard_data/OSR_us_000_0010_8k.wav')
+
     print('sample_rate : ',sample_rate,' and number of samples : ',data.shape[0])
+
+    # optional code for plotting of the signal
+    plot_data(data)
