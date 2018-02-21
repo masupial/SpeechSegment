aefe0b6f305bbe2662b54c0837c109a6e3ac9cb9
masupial <mali@cs.toronto.edu>
2018-02-20 21:38:24 -0500

    dimensionality reduction
    tried a couple of dim reduction techniques, they did not help. need to
    be researched more.

--- Spectrogram.py
+++ Spectrogram.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy import signal
 from ReadData import read_wav_file
 from sklearn.cluster import KMeans
+from sklearn.decomposition import IncrementalPCA
 
 def plot_data_with_spectrogram(data,sample_rate):
     '''
@@ -29,9 +30,9 @@ def plot_data_with_spectrogram(data,sample_rate):
     # return the axis for later use by parent!
     return ax1
 
-def identify_spectrogram_clusters(data,sample_rate,clusters=2):
+def identify_spectrogram_clusters_1(data,sample_rate,clusters=2):
     '''
-    trying k means clustering of the the vertical lines
+    trying k means clustering of the the vertical lines directly
     '''
 
     # hacked the plt.specgram to get the specrogram
@@ -39,6 +40,31 @@ def identify_spectrogram_clusters(data,sample_rate,clusters=2):
 
     # split the Pxx into two clusters
     kmeans = KMeans(n_clusters=2, random_state=0).fit(np.transpose(Pxx))
+
+    return kmeans.labels_,bins
+
+def identify_spectrogram_clusters_2(data,sample_rate,clusters=2):
+    '''
+    trying k means clustering of derived features
+    derived from : extracted version of the vertical lines
+    '''
+
+    # hacked the plt.specgram to get the spectrogram
+    Pxx, freqs, bins, im = plt.specgram(data, Fs=sample_rate)
+
+    # extract Pxx into some lower dimension
+    # first, let's sum of squares
+    spectral_energy_feature = np.sum(Pxx**2, axis=0)
+    #hack: add an extra meaningless dimension to this to have a smooth run
+    spectral_energy_feature = np.stack((np.ones((spectral_energy_feature.shape[0])),spectral_energy_feature),axis=1)
+
+    # pca?, maybe later!
+    #ipca = IncrementalPCA(n_components=2, batch_size=3)
+    #ipca.fit(Pxx)
+    #Pxx_transform = ipca.transform(Pxx)
+
+    # split the Pxx into two clusters
+    kmeans = KMeans(n_clusters=2, random_state=0).fit(spectral_energy_feature)
 
     return kmeans.labels_,bins
 
@@ -51,7 +77,7 @@ if __name__ == '__main__':
     ax_plot = plot_data_with_spectrogram(data[1:np.int(number_samples/5)],sample_rate)
 
     # identifying the clusters
-    labels, bins = identify_spectrogram_clusters(data[1:np.int(number_samples / 5)], sample_rate)
+    labels, bins = identify_spectrogram_clusters_2(data[1:np.int(number_samples / 5)], sample_rate)
     # print(labels)
     for ii in range(labels.shape[0]):
         if labels[ii] == 1:
